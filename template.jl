a8050d03d340b05bc5c98ff55a534f8f3aa82a78
vinsis <sisodia.vinay@gmail.com>
2021-10-23 15:34:20 +0800

    added weights (saturation values)

--- template.jl
+++ template.jl
@@ -1,18 +1,18 @@
 include("sector.jl")
 
-templates = Dict(
-        "i" => [ Sector(0.0,18) ],
-        "V" => [ Sector(0.0,93.6) ],
-        "L" => [ Sector(-39.6,39.6), Sector(81.0,99.0) ],
-        "I" => [ Sector(-9.0,9.0), Sector(171.0,189.0) ],
-        "T" => [ Sector(0.0,180.0) ],
-        "Y" => [ Sector(-9.0,9.0), Sector(140.4,219.6) ],
-        "X" => [ Sector(-39.6,39.6), Sector(140.4,219.6) ]
-    )
+templates = [
+        (name="i", template=[ Sector(0.0,18) ]),
+        (name="V", template=[ Sector(0.0,93.6) ]),
+        (name="L", template=[ Sector(-39.6,39.6), Sector(81.0,99.0) ]),
+        (name="I", template=[ Sector(-9.0,9.0), Sector(171.0,189.0) ]),
+        (name="T", template=[ Sector(0.0,180.0) ]),
+        (name="Y", template=[ Sector(-9.0,9.0), Sector(133.2,226.8) ]),
+        (name="X", template=[ Sector(-46.6,46.6), Sector(133.2,226.8) ])
+    ]
 
 import Statistics: mean
 const Template = Array{S,1} where S<:Sector
-mean_distance(degrees::Array{<:Degree}, t::Template) = mean(distance(d,t) for d in degrees)
+mean_distance(degrees::Array{<:Degree}, t::Template, weights::Array{<:Real}) = mean([distance(d,t) for d in degrees] .* weights)
 
 # find the angle by which sectors in a template must be rotated to get the minimum mean distance
-get_min_mean_dist_and_angle(degrees::Array{<:Degree}, t::Template) = findmin([mean_distance(degrees, t .+ d) for d=0:359])
+get_min_mean_dist_and_angle(degrees::Array{<:Degree}, t::Template, weights::Array{<:Real}) = findmin([mean_distance(degrees, t .+ d, weights) for d=0:359])
